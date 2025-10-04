f2a3d43db872bb129badb6a35de6f4f6574409df
Sagiri0213 <apisittolek18@gmail.com>
2025-10-05 01:11:30 +0700

    Add base URL to fix routing

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // **เพิ่มบรรทัดนี้เพื่อตั้งค่า Base URL**
+  base: '/', 
   plugins: [react()],
 })
